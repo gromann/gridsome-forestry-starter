2781287531b44553bb86f25cd05f8c08f81726e4
Alexander Grossmann <alexander.grossmann@smartbricks.at>
2021-03-10 18:14:12 +0100

    cookie dialog + analytics

--- gridsome.config.js
+++ gridsome.config.js
@@ -22,7 +22,7 @@ module.exports = {
           externalLinksTarget: "_blank",
           externalLinksRel: ["nofollow", "noopener", "noreferrer"]
         }
-      }
+      },
     },
     {
       use: "@gridsome/source-filesystem",
@@ -34,6 +34,43 @@ module.exports = {
           externalLinksTarget: "_blank",
           externalLinksRel: ["nofollow", "noopener", "noreferrer"]
         }
+      }
+    },
+    {
+      use: 'klaro-gridsome',
+      options: {
+        privacyPolicy: '/privacy-policy/',
+        cookieName: 'consent',
+        translations: {
+          en: {
+            consentModal: {
+              description: 'Here you can see and customize the information that we collect about you.',
+            },
+            googleAnalytics: {
+              description: 'Website analytics powered by Google Analytics, allowing us to see how visitors use our website.'
+            },
+            purposes: {
+              analytics: 'Analytics'
+            },
+          },
+        },
+        apps: [
+          {
+            name: 'googleAnalytics',
+            default: true,
+            title: 'Google Analytics',
+            purposes: ['analytics'],
+            cookies: [
+              '_ga',
+              '_gcl_au',
+              '_gid',
+              '_gat'
+            ],
+            required: false,
+            optOut: false,
+            onlyOnce: false
+          }
+        ]
       }
     }
   ],
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="footer">
-        <cookie-law theme="dark-lime"></cookie-law>
+<!--        <cookie-law theme="dark-lime"></cookie-law>-->
         <div class="container">
             <span v-html="settings.footer" />
         </div>
@@ -8,15 +8,37 @@
 </template>
 
 <script>
-    import CookieLaw from 'vue-cookie-law'
+    // import CookieLaw from 'vue-cookie-law'
+    // import CookieLaw from "../../"
 
     export default {
-        components: { CookieLaw },
+        // components: { CookieLaw },
         data() {
         return {
             settings: require("../../data/theme.json")
         }
-    }
+    },
+        // handle cookies
+        mounted() {
+            document.addEventListener('consentUpdate', this.consentToggle)
+        },
+        beforeDestroy() {
+            document.removeEventListener('consentUpdate', this.consentToggle)
+        },
+        methods: {
+            consentToggle(event) {
+                // only is app is google analytics
+                if (event.detail.app === 'googleAnalytics') {
+                    if (event.detail.consent) {
+                        // if user consent is true
+                        this.$ga.enable()
+                    } else {
+                        // if user consent is false
+                        this.$ga.disable()
+                    }
+                }
+            }
+        }
 }
 </script>
 
